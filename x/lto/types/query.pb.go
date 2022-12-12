ef4a7840d0946a97098758beeb0e16c3785b9801
bekauz <bekauz@protonmail.com>
2022-12-12 18:01:01 +0100

    basic query for network state

--- x/lto/types/query.pb.go
+++ x/lto/types/query.pb.go
@@ -113,33 +113,119 @@ func (m *QueryParamsResponse) GetParams() Params {
 	return Params{}
 }
 
+type QueryNetworkTypeRequest struct {
+}
+
+func (m *QueryNetworkTypeRequest) Reset()         { *m = QueryNetworkTypeRequest{} }
+func (m *QueryNetworkTypeRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryNetworkTypeRequest) ProtoMessage()    {}
+func (*QueryNetworkTypeRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d2f32ee82163a3ad, []int{2}
+}
+func (m *QueryNetworkTypeRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryNetworkTypeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryNetworkTypeRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryNetworkTypeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryNetworkTypeRequest.Merge(m, src)
+}
+func (m *QueryNetworkTypeRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryNetworkTypeRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryNetworkTypeRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryNetworkTypeRequest proto.InternalMessageInfo
+
+type QueryNetworkTypeResponse struct {
+	Text string `protobuf:"bytes,1,opt,name=text,proto3" json:"text,omitempty"`
+}
+
+func (m *QueryNetworkTypeResponse) Reset()         { *m = QueryNetworkTypeResponse{} }
+func (m *QueryNetworkTypeResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryNetworkTypeResponse) ProtoMessage()    {}
+func (*QueryNetworkTypeResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d2f32ee82163a3ad, []int{3}
+}
+func (m *QueryNetworkTypeResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryNetworkTypeResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryNetworkTypeResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryNetworkTypeResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryNetworkTypeResponse.Merge(m, src)
+}
+func (m *QueryNetworkTypeResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryNetworkTypeResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryNetworkTypeResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryNetworkTypeResponse proto.InternalMessageInfo
+
+func (m *QueryNetworkTypeResponse) GetText() string {
+	if m != nil {
+		return m.Text
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*QueryParamsRequest)(nil), "lto.lto.QueryParamsRequest")
 	proto.RegisterType((*QueryParamsResponse)(nil), "lto.lto.QueryParamsResponse")
+	proto.RegisterType((*QueryNetworkTypeRequest)(nil), "lto.lto.QueryNetworkTypeRequest")
+	proto.RegisterType((*QueryNetworkTypeResponse)(nil), "lto.lto.QueryNetworkTypeResponse")
 }
 
 func init() { proto.RegisterFile("lto/lto/query.proto", fileDescriptor_d2f32ee82163a3ad) }
 
 var fileDescriptor_d2f32ee82163a3ad = []byte{
-	// 274 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xce, 0x29, 0xc9, 0xd7,
-	0x07, 0xe1, 0xc2, 0xd2, 0xd4, 0xa2, 0x4a, 0xbd, 0x82, 0xa2, 0xfc, 0x92, 0x7c, 0x21, 0xf6, 0x9c,
-	0x92, 0x7c, 0xbd, 0x9c, 0x92, 0x7c, 0x29, 0x91, 0xf4, 0xfc, 0xf4, 0x7c, 0xb0, 0x98, 0x3e, 0x88,
-	0x05, 0x91, 0x96, 0x92, 0x49, 0xcf, 0xcf, 0x4f, 0xcf, 0x49, 0xd5, 0x4f, 0x2c, 0xc8, 0xd4, 0x4f,
-	0xcc, 0xcb, 0xcb, 0x2f, 0x49, 0x2c, 0xc9, 0xcc, 0xcf, 0x2b, 0x86, 0xca, 0x6a, 0x25, 0xe7, 0x17,
-	0xe7, 0xe6, 0x17, 0xeb, 0x27, 0x25, 0x16, 0xa7, 0x42, 0x4c, 0xd5, 0x2f, 0x33, 0x4c, 0x4a, 0x2d,
-	0x49, 0x34, 0xd4, 0x2f, 0x48, 0x4c, 0xcf, 0xcc, 0x03, 0x2b, 0x86, 0xaa, 0x15, 0x81, 0xd9, 0x5e,
-	0x90, 0x58, 0x94, 0x98, 0x0b, 0x35, 0x41, 0x49, 0x84, 0x4b, 0x28, 0x10, 0xa4, 0x2f, 0x00, 0x2c,
-	0x18, 0x94, 0x5a, 0x58, 0x9a, 0x5a, 0x5c, 0xa2, 0xe4, 0xc2, 0x25, 0x8c, 0x22, 0x5a, 0x5c, 0x90,
-	0x9f, 0x57, 0x9c, 0x2a, 0xa4, 0xcb, 0xc5, 0x06, 0xd1, 0x2c, 0xc1, 0xa8, 0xc0, 0xa8, 0xc1, 0x6d,
-	0xc4, 0xaf, 0x07, 0x75, 0xbc, 0x1e, 0x44, 0xa1, 0x13, 0xcb, 0x89, 0x7b, 0xf2, 0x0c, 0x41, 0x50,
-	0x45, 0x46, 0xa9, 0x5c, 0xac, 0x60, 0x53, 0x84, 0x62, 0xb8, 0xd8, 0x20, 0x0a, 0x84, 0xa4, 0xe1,
-	0x3a, 0x30, 0x6d, 0x95, 0x92, 0xc1, 0x2e, 0x09, 0xb1, 0x5c, 0x49, 0xbc, 0xe9, 0xf2, 0x93, 0xc9,
-	0x4c, 0x82, 0x42, 0xfc, 0xfa, 0xa8, 0x1e, 0x71, 0xd2, 0x3c, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23,
-	0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6,
-	0x63, 0x39, 0x86, 0x28, 0x7e, 0x90, 0xaa, 0x0a, 0xb0, 0xda, 0x92, 0xca, 0x82, 0xd4, 0xe2, 0x24,
-	0x36, 0xb0, 0xa7, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0xa1, 0x65, 0x74, 0xeb, 0x8a, 0x01,
-	0x00, 0x00,
+	// 342 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x91, 0xd1, 0x4a, 0x32, 0x41,
+	0x14, 0xc7, 0x77, 0x3f, 0xfc, 0x8c, 0xc6, 0x0b, 0x69, 0x34, 0xb4, 0xcd, 0x36, 0xdb, 0xab, 0x0a,
+	0xda, 0x41, 0x7b, 0x03, 0xe9, 0x3a, 0x4a, 0xba, 0x8a, 0x20, 0xc6, 0x18, 0x16, 0x69, 0xdd, 0x33,
+	0xee, 0x1c, 0x4b, 0x6f, 0x7b, 0x82, 0xa0, 0x97, 0xf2, 0x52, 0xe8, 0xc6, 0xab, 0x08, 0xed, 0x41,
+	0x62, 0x67, 0x46, 0x49, 0x36, 0xba, 0x58, 0x18, 0xce, 0xf9, 0x9d, 0xff, 0x6f, 0xce, 0x0e, 0xa9,
+	0xc4, 0x08, 0x2c, 0xfb, 0x86, 0x23, 0x91, 0x4e, 0x42, 0x99, 0x02, 0x02, 0xdd, 0x8a, 0x11, 0xc2,
+	0x18, 0xc1, 0xab, 0x46, 0x10, 0x81, 0xae, 0xb1, 0xec, 0x64, 0xda, 0x5e, 0x23, 0x02, 0x88, 0x62,
+	0xc1, 0xb8, 0xec, 0x33, 0x9e, 0x24, 0x80, 0x1c, 0xfb, 0x90, 0x28, 0xdb, 0x3d, 0x7d, 0x00, 0x35,
+	0x00, 0xc5, 0x7a, 0x5c, 0x09, 0x93, 0xca, 0x9e, 0x5a, 0x3d, 0x81, 0xbc, 0xc5, 0x24, 0x8f, 0xfa,
+	0x89, 0x86, 0x2d, 0x5b, 0x5d, 0xd9, 0x25, 0x4f, 0xf9, 0xc0, 0x26, 0x04, 0x55, 0x42, 0xaf, 0xb3,
+	0xb9, 0x2b, 0x5d, 0xec, 0x8a, 0xe1, 0x48, 0x28, 0x0c, 0x2e, 0x48, 0x65, 0xa3, 0xaa, 0x24, 0x24,
+	0x4a, 0xd0, 0x33, 0x52, 0x34, 0xc3, 0x75, 0xb7, 0xe9, 0x1e, 0x97, 0xda, 0xe5, 0xd0, 0x5e, 0x3e,
+	0x34, 0x60, 0xa7, 0x30, 0xfd, 0x38, 0x74, 0xba, 0x16, 0x0a, 0xf6, 0x48, 0x4d, 0xa7, 0x5c, 0x0a,
+	0x7c, 0x86, 0xf4, 0xf1, 0x66, 0x22, 0xc5, 0x4a, 0x10, 0x92, 0x7a, 0xbe, 0x65, 0x2d, 0x94, 0x14,
+	0x50, 0x8c, 0x51, 0x3b, 0xb6, 0xbb, 0xfa, 0xdc, 0x9e, 0xbb, 0xe4, 0xbf, 0x1e, 0xa0, 0x77, 0xa4,
+	0x68, 0x64, 0x74, 0x7f, 0x6d, 0xcf, 0x6f, 0xe0, 0x35, 0x7e, 0x6f, 0x1a, 0x45, 0x50, 0x7b, 0x79,
+	0xff, 0x7a, 0xfb, 0xb7, 0x43, 0xcb, 0x6c, 0xf3, 0xa7, 0xd0, 0x21, 0x29, 0xfd, 0xb8, 0x12, 0x6d,
+	0x6e, 0xa6, 0xe4, 0x17, 0xf1, 0x8e, 0xfe, 0x20, 0xac, 0xec, 0x40, 0xcb, 0x6a, 0x74, 0x77, 0x2d,
+	0x4b, 0x0c, 0x75, 0x8f, 0x13, 0x29, 0x3a, 0x27, 0xd3, 0x85, 0xef, 0xce, 0x16, 0xbe, 0xfb, 0xb9,
+	0xf0, 0xdd, 0xd7, 0xa5, 0xef, 0xcc, 0x96, 0xbe, 0x33, 0x5f, 0xfa, 0xce, 0x6d, 0x39, 0x63, 0xc7,
+	0x7a, 0x22, 0x23, 0x55, 0xaf, 0xa8, 0xdf, 0xec, 0xfc, 0x3b, 0x00, 0x00, 0xff, 0xff, 0x28, 0xa8,
+	0xd8, 0x93, 0x49, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -156,6 +242,8 @@ const _ = grpc.SupportPackageIsVersion4
 type QueryClient interface {
 	// Parameters queries the parameters of the module.
 	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
+	// Queries a list of NetworkType items.
+	NetworkType(ctx context.Context, in *QueryNetworkTypeRequest, opts ...grpc.CallOption) (*QueryNetworkTypeResponse, error)
 }
 
 type queryClient struct {
@@ -175,10 +263,21 @@ func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts .
 	return out, nil
 }
 
+func (c *queryClient) NetworkType(ctx context.Context, in *QueryNetworkTypeRequest, opts ...grpc.CallOption) (*QueryNetworkTypeResponse, error) {
+	out := new(QueryNetworkTypeResponse)
+	err := c.cc.Invoke(ctx, "/lto.lto.Query/NetworkType", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// Parameters queries the parameters of the module.
 	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
+	// Queries a list of NetworkType items.
+	NetworkType(context.Context, *QueryNetworkTypeRequest) (*QueryNetworkTypeResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -187,6 +286,9 @@ type UnimplementedQueryServer struct {
 
 func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsRequest) (*QueryParamsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Params not implemented")
+}
+func (*UnimplementedQueryServer) NetworkType(ctx context.Context, req *QueryNetworkTypeRequest) (*QueryNetworkTypeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NetworkType not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -211,6 +313,24 @@ func _Query_Params_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_NetworkType_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryNetworkTypeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).NetworkType(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/lto.lto.Query/NetworkType",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).NetworkType(ctx, req.(*QueryNetworkTypeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "lto.lto.Query",
 	HandlerType: (*QueryServer)(nil),
@@ -218,6 +338,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Params",
 			Handler:    _Query_Params_Handler,
+		},
+		{
+			MethodName: "NetworkType",
+			Handler:    _Query_NetworkType_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -280,6 +404,59 @@ func (m *QueryParamsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryNetworkTypeRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryNetworkTypeRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryNetworkTypeRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryNetworkTypeResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryNetworkTypeResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryNetworkTypeResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Text) > 0 {
+		i -= len(m.Text)
+		copy(dAtA[i:], m.Text)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.Text)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -308,6 +485,28 @@ func (m *QueryParamsResponse) Size() (n int) {
 	_ = l
 	l = m.Params.Size()
 	n += 1 + l + sovQuery(uint64(l))
+	return n
+}
+
+func (m *QueryNetworkTypeRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *QueryNetworkTypeResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Text)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
 	return n
 }
 
@@ -428,6 +627,138 @@ func (m *QueryParamsResponse) Unmarshal(dAtA []byte) error {
 			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryNetworkTypeRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryNetworkTypeRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryNetworkTypeRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryNetworkTypeResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryNetworkTypeResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryNetworkTypeResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Text", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Text = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
