7542cae9d2e67af732dfc52aa05d2effa98ef565
bekauz <bekauz@protonmail.com>
2023-03-02 15:24:56 +0100

    rescaffolding chain with no modules

--- ts-client/lto.lto/registry.ts
+++
@@ -1,9 +0,0 @@
-import { GeneratedType } from "@cosmjs/proto-signing";
-import { MsgCreateAnchor } from "./types/lto/lto/tx";
-
-const msgTypes: Array<[string, GeneratedType]>  = [
-    ["/lto.lto.MsgCreateAnchor", MsgCreateAnchor],
-    
-];
-
-export { msgTypes }
--- ts-client/lto.lto/types.ts
+++
@@ -1,9 +0,0 @@
-import { Anchor } from "./types/lto/lto/anchor"
-import { Params } from "./types/lto/lto/params"
-
-
-export {     
-    Anchor,
-    Params,
-    
- }
