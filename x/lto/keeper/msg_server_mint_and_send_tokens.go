e0231fad10a7bff93abde07c28ed9e25ab0fa78e
bekauz <bekauz@protonmail.com>
2023-03-07 15:01:24 +0100

    methods for minting and sending denoms; updating owner

--- x/lto/keeper/msg_server_mint_and_send_tokens.go
+++ x/lto/keeper/msg_server_mint_and_send_tokens.go
@@ -3,15 +3,69 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"lto-cosmos/x/lto/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) MintAndSendTokens(goCtx context.Context, msg *types.MsgMintAndSendTokens) (*types.MsgMintAndSendTokensResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// check if the value exists
+	valFound, isFound := k.GetDenom(
+		ctx,
+		msg.Denom,
+	)
+
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "denom not found")
+	}
+
+	// check if the new msg owner is the same as the current owner
+	if msg.Owner != valFound.Owner {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+
+	// check if amount does not exceed the max supply
+	if msg.Amount+valFound.Supply > valFound.MaxSupply {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "mint amount would exceed max supply")
+	}
+
+	moduleAcct := k.accountKeeper.GetModuleAddress(types.ModuleName)
+
+	recipientAddress, err := sdk.AccAddressFromBech32(msg.Recipient)
+	if err != nil {
+		return nil, err
+	}
+
+	var mintCoins sdk.Coins
+
+	mintCoins = mintCoins.Add(sdk.NewCoin(msg.Denom, sdk.NewInt(int64(msg.Amount))))
+	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, mintCoins); err != nil {
+		return nil, err
+	}
+
+	if err := k.bankKeeper.SendCoins(ctx, moduleAcct, recipientAddress, mintCoins); err != nil {
+		return nil, err
+	}
+
+	var denom = types.Denom{
+		Owner:              valFound.Owner,
+		Denom:              valFound.Denom,
+		Description:        valFound.Description,
+		MaxSupply:          valFound.MaxSupply,
+		Supply:             valFound.Supply + msg.Amount,
+		Precision:          valFound.Precision,
+		Ticker:             valFound.Ticker,
+		Url:                valFound.Url,
+		CanChangeMaxSupply: valFound.CanChangeMaxSupply,
+	}
+
+	k.SetDenom(
+		ctx,
+		denom,
+	)
 
 	return &types.MsgMintAndSendTokensResponse{}, nil
 }
--- x/lto/keeper/msg_server_update_owner.go
+++ x/lto/keeper/msg_server_update_owner.go
@@ -3,15 +3,48 @@ package keeper
 import (
 	"context"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"lto-cosmos/x/lto/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) UpdateOwner(goCtx context.Context, msg *types.MsgUpdateOwner) (*types.MsgUpdateOwnerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	valFound, isFound := k.GetDenom(
+		ctx,
+		msg.Denom,
+	)
+
+	// check for the existance of denom
+	if !isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "denom not found")
+	}
+
+	// check for owner
+	if valFound.Owner != msg.Owner {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "only owner can update")
+	}
+
+	// update the appropriate fields of the denom
+	var denom = types.Denom{
+		Owner:              msg.NewOwner,
+		Denom:              msg.Denom,
+		Description:        valFound.Description,
+		MaxSupply:          valFound.MaxSupply,
+		Supply:             valFound.Supply,
+		Precision:          valFound.Precision,
+		Ticker:             valFound.Ticker,
+		Url:                valFound.Url,
+		CanChangeMaxSupply: valFound.CanChangeMaxSupply,
+	}
+
+	// save denom in the keeper
+	k.SetDenom(
+		ctx,
+		denom,
+	)
 
 	return &types.MsgUpdateOwnerResponse{}, nil
 }
